7f1369f4a2510ad72f7db5be39ef12f4a08c861d
bruno-coulet <147488292+bruno-coulet@users.noreply.github.com>
2023-11-15 09:43:34 +0100

    myupdate.sh en une ligne

--- Job02/myupdate.sh
+++ Job02/myupdate.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+# ceci est un commentaire, la ligne du dessus est un shibang qui indique à linux quek interpreteur utiliser.
+
+# ci dessous le code en une seule ligne
+# le && indique que si l'instruction d'avant && est éxécutée correctement, il faut alors executer l'instruction après && :
+
+sudo apt update && sudo apt upgrade
+
+# ci dessous l'ancien code en 2 lignes, il n'en fallait qu'une pour répondre à l'exercice :
 sudo apt update
 sudo apt upgrade
